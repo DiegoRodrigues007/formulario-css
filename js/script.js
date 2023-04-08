f35266e7233d24530f51d61a2a53877be8460a50
Diego Rodrigues <diego.rodrigues3007@gmail.com>
2023-04-08 07:17:35 -0300

    alteração na lógica do código

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
+const nameInput = document.getElementById("nome");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("senha");
+const btn = document.getElementById("btn");
+const textError = document.getElementsByClassName("text-error");
 
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
 
+  const valorNome = nameInput.value;
+  const valorEmail = emailInput.value;
+  const valorSenha = passwordInput.value;
 
-const nameInput = document.getElementById('nome');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('senha');
-const btn = document.getElementById('btn');
-const textError = document.getElementsByClassName('text-error');
-
-
-
-
-btn.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    const valorNome = nameInput.value;
-    const valorEmail = emailInput.value;
-    const valorSenha = passwordInput.value;
-
-    if(valorNome === '' || valorEmail === '' || valorSenha === '') {
-       
-        textError[0].innerHTML = 'Preencha o campo nome';
-        textError[1].innerHTML = 'Preencha o campo email';
-        textError[2].innerHTML = 'Preencha o campo senha';
-    }
-})
+  if (valorNome === "") {
+    textError[0].innerHTML = "Preencha o campo nome";
+  }
+  if (valorEmail === "") {
+    textError[1].innerHTML = "Preencha o campo email";
+  }
+  if (valorSenha === "") {
+    textError[2].innerHTML = "Preencha o campo senha";
+  }
+});
